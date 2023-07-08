1a555622538c9cec8310058a2faac1cfaec7b6ca
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-07-08 11:33:21 +0300

    FileReaderHandler is rdy

--- src/FileReaderHandler.java
+++ src/FileReaderHandler.java
@@ -17,27 +17,9 @@ public class FileReaderHandler {
             }
             sc.close();
         } catch (FileNotFoundException e) {
-            System.out.println("nem jós path");
+            System.out.println("nem jó path");
         }
         return stringList;
     }
 
-
-    public static List<String> getLinesOfTextFile(String filePath) {
-        List<String> stringList = new ArrayList<>();
-
-        try {
-            FileReader fileReader = new FileReader(filePath);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            for (String line; (line = bufferedReader.readLine()) != null; ) {
-                stringList.add(line);
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error: wrong path!\n" + e.getMessage());
-        }
-
-        return stringList;
-    }
 }
